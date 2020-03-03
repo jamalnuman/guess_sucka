be6a4a9045a38f43013229287329d17bd3243ad0
“jamalnuman” <jnuman82@gmail.com>
2020-03-03 17:00:34 -0600

    fixed the code

--- src/views/GuessIndex.vue
+++ src/views/GuessIndex.vue
@@ -4,35 +4,24 @@
 
     <div id="wrapup-page" v-if="isHidden">
       <h1>RESULTS:</h1>
-      <p>You got {{ rightCount }} question(s) correct and {{ wrongCount }} question(s) wrong.</p>
-
-      <div>
-        <ol class="result-lists">
-          <li v-for="feedbackChosenAnswer in choiceArr">
-            You chose {{ feedbackChosenAnswer }} as an answer.
+      <p>You got {{ rightCount}} question(s) correct and {{ wrongCount }} question(s) wrong.</p>
+        <ul class="result-lists" v-for="question in this.questions">
+          <li v-if="question.userChoice === question.correct_answer">
+            Good job, you guessed correctly. You chose {{question.userChoice}}.
           </li>
-        </ol>
-        <ol class="result-lists">
-          <li v-for="feedbackRightAnswer in answerArr">
-            The correct answer is: {{ feedbackRightAnswer }}.
+          <li v-else>
+            You chose {{question.userChoice}}, but the correct answer is {{question.correct_answer}}.
           </li>
-        </ol>
-      </div>
-      
+        </ul>
       <button @click="restartGame">
         Restart Game
       </button>
     </div>
-
     <div id="main-wrapper" v-if="!isHidden">
       <div id="question-wrapper">
         <h2>Question:</h2>
         <h3> {{ questions[incrementer].question }} </h3>
-        <!-- <div v-for="fQuestion in questions[incrementer][0]">
-            {{ fQuestion }}
-        </div> -->
       </div>
-
       <div id="choice-wrapper">
         <select id="questions-select-box" v-model="questions[incrementer].userChoice">
           <option v-for="choice in questions[incrementer].incorrect_answers">
@@ -40,7 +29,6 @@
           </option>
         </select>
       </div>
-
       <div id="button-wrapper">
         <button id="submit-button" @click="submitAnswer">
           Submit Answer
@@ -62,12 +50,12 @@ export default {
     return {
       questions: [
                   { 
-                    "category": "Entertainment: Film", 
-                    "type": "multiple", 
-                    "difficulty": "easy", 
-                    "question": "Which of the following was not one of &quot;The Magnificent Seven&quot;?", 
-                    "correct_answer": "Clint Eastwood", 
-                    "incorrect_answers": [ "Steve McQueen", "Charles Bronson", "Robert Vaughn" ] 
+                    "category": "", 
+                    "type": "", 
+                    "difficulty": "", 
+                    "question": "", 
+                    "correct_answer": "", 
+                    "incorrect_answers": [] 
                   }
                 ],
       isHidden: false,
@@ -78,10 +66,9 @@ export default {
   },
   created: function() {
     axios
-      .get("https://opentdb.com/api.php?amount=3&category=11&difficulty=easy&type=multiple")
+      .get("https://opentdb.com/api.php?amount=4&category=11&difficulty=easy&type=multiple")
       .then(response => {
         this.questions = response.data.results;
-
         this.questions.forEach(function(question) {
           question.incorrect_answers.push(question.correct_answer).shuffle;
         });
@@ -95,7 +82,6 @@ export default {
       } else {
         this.wrongCount++;
       }
-
       if (this.incrementer < this.questions.length - 1) {
         this.incrementer++;
       } else if (this.incrementer === this.questions.length - 1) {
@@ -105,6 +91,7 @@ export default {
     restartGame: function() {
       this.incrementer = 0;
       this.isHidden = false;
+      this.$router.push('/')
     }
   }
 };
